e4b107abc4562cc26dd6d386c957cc9c79a2e2ae
WeijieChen2017 <weijiechen1997@gmail.com>
2021-08-29 10:18:20 -0500

    v1

--- deeprad_keras_tools.py
+++ deeprad_keras_tools.py
@@ -96,6 +96,8 @@ class SimpleKerasGenerator(Sequence):
         batch_x_fns = self.X_filenames[idx * self.batch_size:(idx + 1) * self.batch_size]
         batch_y_fns = self.Y_filenames[idx * self.batch_size:(idx + 1) * self.batch_size]
 
+        print(batch_x_fns, batch_y_fns)
+
         batch_x = np.array( [ imread(fn) for fn in batch_x_fns ] )
         batch_x = np.expand_dims(batch_x,3)
         batch_y = np.array( [ imread(fn) for fn in batch_y_fns ] )
