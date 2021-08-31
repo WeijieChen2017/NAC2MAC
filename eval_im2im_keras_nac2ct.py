2d05445f2bf595d2030feacfd2018123ab29165d
WeijieChen2017 <weijiechen1997@gmail.com>
2021-08-31 09:59:18 -0500

    v1

--- eval_im2im_keras_nac2ct.py
+++ eval_im2im_keras_nac2ct.py
@@ -24,7 +24,7 @@ def smooth_L1_loss(y_true, y_pred):
     return losses.huber(y_true, y_pred)
 
 def execute():
-    data_in_chan = 1
+    data_in_chan = 5
     data_out_chan = 1
     data_x = 512
     data_y = 512*data_in_chan    
@@ -38,13 +38,23 @@ def execute():
     Y_folder = "./data_train/Y/"
 
     print('creating model')
-    model = Unet.UNetContinuous([model_x,model_y,data_in_chan],out_ch=data_out_chan,start_ch=64,depth=4,inc_rate=2.,activation='relu',dropout=0.5,batchnorm=True,maxpool=True,upconv=True,residual=False)
-    model = deeprad_keras_tools.wrap_model( model, (data_x,data_y,1), (data_x,data_y,1), (model_x,model_y,1), (model_x,model_y,1) )    
+    # model = Unet.UNetContinuous([model_x,model_y,data_in_chan],out_ch=data_out_chan,start_ch=64,depth=4,inc_rate=2.,activation='relu',dropout=0.5,batchnorm=True,maxpool=True,upconv=True,residual=False)
+    model = Unet.UNetContinuous([model_x,model_y,data_in_chan],
+                                out_ch=data_out_chan,
+                                start_ch=64, depth=4, inc_rate=2,
+                                activation='relu', dropout=0.5,
+                                batchnorm=False, maxpool=True, # turn off batchnorm
+                                upconv=True, residual=False)
+
+
+    # model = deeprad_keras_tools.wrap_model( model, (data_x,data_y,1), (data_x,data_y,1), (model_x,model_y,1), (model_x,model_y,1) )    
     model.compile(optimizer=Adam(learning_rate=1e-4), loss=smooth_L1_loss, metrics=[smooth_L1_loss,losses.mean_squared_error,losses.mean_absolute_error])
     model.summary()
 
     print('creating data generators')
-    test_gen = deeprad_keras_tools.get_keras_tiff_generator( os.path.join(X_folder,'test'), os.path.join(Y_folder,'test'), batch_size )
+    test_gen = deeprad_keras_tools.get_keras_tiff_generator( os.path.join(X_folder,'test'),
+                                                             os.path.join(Y_folder,'test'),
+                                                             batch_size )
     
     print('load model weights')
     model.load_weights("./" + model_name + "_model.h5")
